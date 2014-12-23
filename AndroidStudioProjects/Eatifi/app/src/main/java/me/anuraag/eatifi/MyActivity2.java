6982700ebe274f255b9ac04e28908a1e331b0a31
Anuraag Yachamaneni <hello@anuraag.me>
2014-12-23 11:17:26 -0500

    add new code

---
+++ AndroidStudioProjects/Eatifi/app/src/main/java/me/anuraag/eatifi/MyActivity2.java
@@ -0,0 +1,54 @@
+package me.anuraag.eatifi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.andtinder.model.CardModel;
+import com.andtinder.model.Orientations;
+import com.andtinder.view.CardContainer;
+import com.andtinder.view.SimpleCardStackAdapter;
+
+
+public class MyActivity2 extends Activity {
+    private CardContainer mycards;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_activity2);
+        mycards = (CardContainer)findViewById(R.id.layoutview);
+        mycards.setOrientation(Orientations.Orientation.Ordered);
+        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this);
+        CardModel card = new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.background));
+        adapter.add(card);
+
+        adapter.add(new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.background)));
+        adapter.add(new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.background)));
+        adapter.add(new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.background)));
+        adapter.add(new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.background)));
+        adapter.add(new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.background)));
+        mycards.setAdapter(adapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
